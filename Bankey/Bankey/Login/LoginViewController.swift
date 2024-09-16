4fc7a642876e30b4e3c86c0936377b336b0cd5c1
Siemens Co-op <siemens.coop.1@gmail.com>
2024-09-16 09:52:37 -0400

    Added LoginView and Code Snippets, learned about setting up LoginView w/ anchors

---
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Scott Karstetter on 9/11/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    let loginView = LoginView()
+    	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension LoginViewController {
+    private func style() {
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+        loginView.backgroundColor = .orange
+    }
+    
+    private func layout() {
+        view.addSubview(loginView)
+        
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
+
