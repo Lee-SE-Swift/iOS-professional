3509c834a422b4de4d4e906bd8fe505f00eb63f5
Siemens Co-op <siemens.coop.1@gmail.com>
2024-11-04 14:57:49 -0500

    feat: Add tab bar

---
+++ Bankey/Bankey/Utils/UIViewController+Utils.swift
@@ -0,0 +1,26 @@
+//
+//  UIViewController+Utils.swift
+//  Bankey
+//
+//  Created by Scott Karstetter on 10/30/24.
+//
+
+import UIKit
+
+extension UIViewController {
+    // New
+    func setStatusBar() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground() // to hide Navigation Bar Line also
+        navBarAppearance.backgroundColor = appColor
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+    }
+    
+    
+    func setTabBarImage(imageName: String, title: String) {
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+}
